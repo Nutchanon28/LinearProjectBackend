ff04fb1f4305b4ed78bc80fff4da00ef6405b1fd
Jetsarit Kaewsaart <applerew14112546@gmail.com>
2023-10-13 20:33:36 +0700

    first commit inpainting

---
+++ inpainter/__init__.py
@@ -0,0 +1,16 @@
+"""python inpainter <path to image> <path to mask>
+python inpainter ./image1.jpg ./mask1.jpg"""
+
+
+
+
+"""
+เริ่มต้น 
+pip install -r requirements.txt
+
+รันโค้ด
+python inpainter <path to image> <path to mask>
+เช่น
+python inpainter ./image1.jpg ./mask1.jpg
+
+"""
